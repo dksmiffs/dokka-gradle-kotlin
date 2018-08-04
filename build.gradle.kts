9ef2b1635c686c0bb600f12556c13887e956af60
Dave Smith <dave.k.smith@gmail.com>
2018-08-04 13:27:34 -0500

    introduce Dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,17 @@
 // github.com guidance:
 //    gradle/kotlin-dsl/blob/master/samples/hello-kotlin/build.gradle.kts
+//    sureshg/kotlin-starter/blob/master/build.gradle.kts
+//    eriwen/gradle-digest-plugin/blob/master/build.gradle.kts
+import org.jetbrains.dokka.gradle.DokkaTask
+
 plugins {
   application
+  id("org.jetbrains.dokka") version "0.9.17"
   kotlin("jvm") version "1.2.60"
 }
 
 application {
-  mainClassName = "HelloKt"
+  mainClassName = "HelloDokka"
 }
 
 dependencies {
@@ -17,7 +22,12 @@ repositories {
   jcenter()
 }
 
-// github.com guidance:
-//    sureshg/kotlin-starter/blob/master/build.gradle.kts
 defaultTasks("run")
+
+val dokka by tasks.getting(DokkaTask::class) {
+  outputFormat = "html"
+  outputDirectory = "$buildDir/javadoc"
+  jdkVersion = 8
+  reportUndocumented = true
+}
 
--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,4 +1,14 @@
-fun main(args : Array<String>) {
-    println("Hello, World!")
+/**
+ * A cliche of a program, written only for these KDoc bits.
+ *
+ * A detailed description goes here.
+ */
+class HelloDokka {
+  companion object {
+    @JvmStatic
+    fun main(vararg args: String) {
+      println("Hello, Dokka!")
+    }
+  }
 }
 
